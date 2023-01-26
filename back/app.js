7e3b357b77201dc68af5d3e9b4a32cf7e71bd425
Laure Lozano <laurelozano@gmail.com>
2023-01-26 14:34:51 +0100

    express rate limit modified

--- back/app.js
+++ back/app.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 
 // Initializing the Per-User Request Limiter
 limiter = rateLimit({
-    windowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
+    windowMs: 60 * 60 * 1000, // 24 hrs in milliseconds
     max: 100,
     message: 'You have exceeded the 100 requests in 24 hrs limit!', 
     standardHeaders: true,
